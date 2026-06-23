2402e9d0bf79c667322ab07f0beec1cfec758a36
Karthickraja <karthickraja.r@avantaifintech.com>
2026-06-23 18:10:26 +0400

    created a model and made a controller functionalities

--- controllers/movie.contoller.js
+++ controllers/movie.contoller.js
@@ -1,15 +1,68 @@
-  export const ListMovies = (req, res) => {
-    res.send("Get all movies");
+import Movie from "../models/movie.model.js";
+
+  export const ListMovies = async(req, res) => {
+    try {
+        const movie = await Movie.find();
+        res.status(200).json(movie)
+        
+    } catch (error) {
+        res.status(400).json({message : error?.message})
+    }
   }
 
-  export const AddMovie = (req,res) => {
+  export const MovieDetail = async(req,res) => {
+    try {
+        const movie = await Movie.findById(req?.params?.id)
+        res.status(201).json(movie)
+        
+    } catch (error) {
+        res.status(404).json({message : error.message})
+    }
+  }
 
+  export const AddMovie = async(req,res) => {
+
+    const newMovie = new Movie({
+        title : req.body.title,
+        desc : req.body.desc
+    })
+
+    try {        
+        const movie = await newMovie.save();
+        res.status(200).json(movie)
+
+    } catch (error) {
+        res.status(400).json({message : error?.message})
+    }
+    
   }
   
-  export const UpdateMovie = (req,res) => {
+  export const UpdateMovie = async(req,res) => {
+
+    try {
+        const movie = await Movie.findOneAndUpdate({_id : req?.params?.id}, {
+            title : req.body.title,
+            desc : req.body.desc
+        },
+        {
+            new : true
+        }
+    )
+        res.status(200).json(movie)
+        
+    } catch (error) {
+        res.status(400).json({message : error?.message})
+    }
+
       
   }
   
-  export const DeleteMovie = (req,res) => {
-      
+  export const DeleteMovie = async(req,res) => {
+    try {
+        await Movie.deleteOne({_id:req?.params?.id});
+        res.status(200).json({message : "Movie Deleted"})
+        
+    } catch (error) {
+        res.status(404).json({message : error?.message})
+    }
   }
--- routes/movies.route.js
+++ routes/movies.route.js
@@ -1,11 +1,13 @@
 import express from "express";
-import { AddMovie, DeleteMovie, ListMovies, UpdateMovie } from "../controllers/movie.contoller.js";
+import { AddMovie, DeleteMovie, ListMovies, UpdateMovie, MovieDetail } from "../controllers/movie.contoller.js";
 
 const router = express.Router();
 
 router.get("/", ListMovies);
 
 router.post('/', AddMovie)
+
+router.get('/:id', MovieDetail)
 
 router.put('/:id', UpdateMovie)
 
